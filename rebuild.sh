7d98ca68a411e0589147aa042895b99796250b4b
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2009-10-08 23:35:33 -0700

    Changed web page generating message to display filename.

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+INDEX_PAGE="/srv/www/org.aedifice/private/htdocs/awstats/index.html"
+
+
 cd `dirname $0`
 echo "Generating configs:"
 ./gen_awstats_conf.sh
 echo ""
-echo "Generating webpage list"
-./gen_awstats_list.rb > /srv/www/org.aedifice/private/htdocs/awstats/index.html
+echo "Generating ${INDEX_PAGE}"
+./gen_awstats_list.rb > ${INDEX_PAGE}
 echo "Done"
