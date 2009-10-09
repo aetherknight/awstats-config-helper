e6cdb003c428619c296018aeebbc564847f8da73
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2009-10-08 23:30:59 -0700

    Changed rebuild.sh to make the generated webpage be /awstats/index.html

--- rebuild.sh
+++ rebuild.sh
@@ -4,5 +4,5 @@ echo "Generating configs:"
 ./gen_awstats_conf.sh
 echo ""
 echo "Generating webpage list"
-./gen_awstats_list.rb > /srv/www/org.aedifice/private/htdocs/awstat_list.html
+./gen_awstats_list.rb > /srv/www/org.aedifice/private/htdocs/awstats/index.html
 echo "Done"
