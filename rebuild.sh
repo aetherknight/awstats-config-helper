be4b2a698cb1af9484a91a3769176b13a0f803ea
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2009-10-08 23:10:12 -0700

    Adding a script to run the other gen_* scripts

---
+++ rebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd `dirname $0`
+echo "Generating configs:"
+./gen_awstats_conf.sh
+echo ""
+echo "Generating webpage list"
+./gen_awstats_list.rb > /srv/www/org.aedifice/private/htdocs/awstat_list.html
+echo "Done"
